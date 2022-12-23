3c741ea959e5ef6bad25412973db9191af65e646
Isaac Priestley <progressions@gmail.com>
2022-12-23 12:31:22 -0500

    Add characters to fight

--- components/AddFight.tsx
+++ components/AddFight.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Paper, Stack, Typography, TextField, Button } from '@mui/material'
-import Router from "next/router";
+import Router from "next/router"
 
 export default function AddFight({ endpoint }: any) {
   const [fight, setFight] = useState({name: ''})
