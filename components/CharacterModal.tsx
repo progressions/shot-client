deee2320bb0951604013f874dd566e073c61cea1
Isaac Priestley <progressions@gmail.com>
2022-12-31 14:44:10 -0500

    Color picker

--- components/CharacterModal.tsx
+++ components/CharacterModal.tsx
@@ -4,10 +4,15 @@ import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import { Paper, Popover } from '@mui/material'
 import Router from "next/router"
 import { useSession } from 'next-auth/react'
+import { BlockPicker, SwatchesPicker } from 'react-color';
 
 export default function CharacterModal(props: any) {
+  const [picker, setPicker] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
+
   const session: any = useSession({ required: true })
   const jwt = session?.data?.authorization
 
@@ -25,6 +30,12 @@ export default function CharacterModal(props: any) {
 
   const handleChange = (event: any) => {
     setCharacter((prevState: any) => ({ ...prevState, [event.target.name]: event.target.value }))
+  }
+
+  const handleColor = (color: any) => {
+    setCharacter((prevState: any) => ({ ...prevState, color: color?.hex }))
+    setPicker(false)
+    setAnchorEl(null)
   }
 
   const cancelForm = () => {
@@ -58,6 +69,16 @@ export default function CharacterModal(props: any) {
     Router.reload()
   }
 
+  const togglePicker = (event: any) => {
+    if (picker) {
+      setPicker(false)
+      setAnchorEl(null)
+    } else {
+      setPicker(true)
+      setAnchorEl(event.target)
+    }
+  }
+
   return (
     <>
       <Dialog
@@ -72,12 +93,18 @@ export default function CharacterModal(props: any) {
             <Stack direction="row">
               <TextField autoFocus label="Name" fullWidth required name="name" value={character.name} onChange={handleChange} />
             </Stack>
-            <Stack spacing={2} direction="row">
+            <Stack spacing={2} direction="row" alignItems='center'>
               <TextField label="Current Shot" name="current_shot" value={character.current_shot || ''} onChange={handleChange} />
               <TextField label="Defense" name="defense" value={character.defense || ''} onChange={handleChange} />
               <TextField label="Impairments" name="impairments" value={character.impairments || ''} onChange={handleChange} />
-              <TextField label="Color" name="color" value={character.color || ''} onChange={handleChange} />
+              <Box p={2} sx={{width: 10, height: 5, bgcolor: character.color, borderColor: 'primary', border: 1, borderRadius: 2}} onClick={togglePicker} />
+              <TextField id="colorPicker" label="Color" name="color" value={character.color || ''} onChange={handleChange} />
             </Stack>
+            <Popover anchorEl={anchorEl} open={picker} onClose={() => setPicker(false)} anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}>
+              <Paper>
+                <BlockPicker color={character.color || ''} onChangeComplete={handleColor} colors={['#D0021B', '#F5A623', '#F8E71C', '#8B572A', '#7ED321', '#417505', '#BD10E0', '#9013FE', '#4A90E2', '#50E3C2', '#B8E986', '#000000', '#4A4A4A', '#9B9B9B', '#FFFFFF']} />
+              </Paper>
+            </Popover>
             <Stack alignItems="flex-end" spacing={2} direction="row">
               <Button variant="outlined" disabled={saving} onClick={cancelForm}>Cancel</Button>
               <Button variant="contained" type="submit" disabled={saving}>Save Changes</Button>
