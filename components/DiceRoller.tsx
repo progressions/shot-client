282c028ecada398784a329e179bf777a42ae9421
Isaac Priestley <progressions@gmail.com>
2023-01-02 17:10:54 -0500

    Mook rolls

--- components/DiceRoller.tsx
+++ components/DiceRoller.tsx
@@ -2,16 +2,16 @@ import { Tooltip, IconButton, Dialog, Box, Typography} from '@mui/material'
 import CasinoIcon from '@mui/icons-material/Casino'
 import { useState } from 'react'
 
-export const roll = () => {
+export const rollDie = () => {
   const result = Math.floor(Math.random() * 6) + 1
   return result
 }
 
-export const explodingRoll = (roll) => {
-  let result = roll()
+export const rollExplodingDie = (rollDie: any) => {
+  let result = rollDie()
   let total = []
   do {
-    result = roll()
+    result = rollDie()
     total.push(result)
   }
   while (result == 6)
@@ -19,22 +19,22 @@ export const explodingRoll = (roll) => {
   return [total, total.reduce((sum, num) => {return (sum + num)}, 0)]
 }
 
+const rollSwerve = () => {
+  const [posRolls, positive] = rollExplodingDie(rollDie)
+  const [negRolls, negative] = rollExplodingDie(rollDie)
+
+  const result = positive - negative
+  console.log({ result })
+
+  return result
+}
+
 export default function DiceRoller({ }) {
   const [open, setOpen] = useState(false)
-  const [result, setResult] = useState(null)
-
-  const swerve = () => {
-    const [posRolls, positive] = explodingRoll(roll)
-    const [negRolls, negative] = explodingRoll(roll)
-
-    const result = positive - negative
-    console.log({ result })
-
-    return result
-  }
+  const [result, setResult] = useState<number | null>(null)
 
   const showExplodingRoll = () => {
-    const sum = swerve()
+    const sum = rollSwerve()
     setResult(sum)
     setOpen(true)
   }
--- components/MookRolls.tsx
+++ components/MookRolls.tsx
@@ -1,18 +1,30 @@
-import { Stack, Dialog, Button, Box, Typography, TextField } from '@mui/material'
+import { Grid, Stack, Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, Button, Box, Typography, TextField } from '@mui/material'
 import { useState } from 'react'
-import { roll, explodingRoll } from './DiceRoller'
+import { rollDie, rollExplodingDie } from './DiceRoller'
 
 export default function MookRolls({ }) {
+  const defaultValue = {count: '10', attack: '8', defense: '13'}
   const [open, setOpen] = useState(false)
-  const [value, setValue] = useState({count: '', attack: 8})
-  const [rolls, setRolls] = useState([])
+  const [value, setValue] = useState(defaultValue)
+  const [rolls, setRolls] = useState<number[]>([])
 
   const handleChange = (event: any) => {
     setValue({...value, [event.target.name]: event.target.value})
   }
 
-  const generateRolls = () => {
-    alert("roll")
+  const handleClose = () => {
+    setOpen(false)
+    setValue(defaultValue)
+    setRolls([])
+  }
+
+  const generateRolls = (event: any) => {
+    event.preventDefault()
+    const count = parseInt(value.count) || 0
+    for (var i = 0; i < count; i++) {
+      const [dieRolls, result] = rollExplodingDie(rollDie)
+      setRolls((oldArray: any) => [...oldArray, (result + parseInt(value.attack))])
+    }
   }
 
   return (
@@ -20,17 +32,47 @@ export default function MookRolls({ }) {
       <Button variant="outlined" onClick={() => setOpen(true)}>Mook Rolls</Button>
       <Dialog
         open={open}
-        onClose={() => setOpen(false)}
+        onClose={handleClose}
+        disableRestoreFocus
       >
-        <Box p={4}>
-          <Stack spacing={2}>
-            <Typography variant="h3">Mook Rolls</Typography>
-            <Stack direction="row" spacing={2}>
-              <TextField name="count" value={value.count} onChange={handleChange} label="Count" required sx={{width: 150}} />
-              <TextField name="attack" value={value.attack} onChange={handleChange} label="Attack Value" required sx={{width: 150}} />
+        <Box component="form" onSubmit={generateRolls}>
+        <DialogTitle>Mook Rolls</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Generate some mook rolls
+          </DialogContentText>
+          <Box py={2} sx={{width: 300}}>
+            <Stack spacing={2}>
+              <Stack direction="row" spacing={2}>
+                <TextField name="count" autoFocus value={value.count} onChange={handleChange} label="Count" required sx={{width: 150}} />
+                <TextField name="attack" value={value.attack} onChange={handleChange} label="Attack Value" required sx={{width: 150}} />
+                <TextField name="defense" value={value.defense} onChange={handleChange} label="Against Defense" required sx={{width: 150}} />
+              </Stack>
             </Stack>
-            <Button variant="contained" onClick={generateRolls}>Roll!</Button>
-          </Stack>
+            <Box py={2}>
+              <Grid container sx={{width: "100%"}}>
+                {
+                  rolls.map((outcome, index) => {
+                    const defense = parseInt(value.defense) || 0
+                    const winner = (outcome >= parseInt(value.defense))
+                    const style = (value.defense && winner) ? {color: 'red', fontWeight: 'bold'} : {}
+                    return (
+                      <Grid key={index} item xs={2}>
+                        <Typography sx={style} variant='h5'>
+                          {outcome}
+                        </Typography>
+                      </Grid>
+                    )
+                  })
+                }
+              </Grid>
+            </Box>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" onClick={handleClose}>Cancel</Button>
+          <Button type="submit" variant="contained">Roll!</Button>
+        </DialogActions>
         </Box>
       </Dialog>
     </>
