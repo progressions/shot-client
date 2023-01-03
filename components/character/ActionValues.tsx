3bf33761deb4b037f30edc42026f24684499bc4f
Isaac Priestley <progressions@gmail.com>
2023-01-03 16:35:15 -0500

    Styles for action values

--- components/character/ActionValues.tsx
+++ components/character/ActionValues.tsx
@@ -1,26 +1,28 @@
 import { TableContainer, Table, TableHead, TableBody, TableCell, TableRow } from '@mui/material'
-import { Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import { tableCellClasses } from "@mui/material/TableCell"
 
 export default function ActionValues({ character }: any) {
   const color = character.impairments ? 'red' : ''
+  const borderColor = "#aaa"
   const styles = {
     width: 60,
-    borderColor: '#ccc',
+    borderColor: borderColor,
     borderRight: 'none'
   }
   const ActionValueDisplay = ({ name, label, character, sx }: any) => {
     if (character.action_values[name]) {
       return (
         <Stack spacing={1} sx={sx || styles} border={1}>
-          <Typography variant="body2" align='center'>{label}</Typography>
-          <Typography variant="h6" align='center' sx={{color: color}}>{character.action_values[name] - (character.impairments || 0)}</Typography>
+          <Typography variant="body2" color="white" align="center"><Box bgcolor={borderColor}>{label}</Box></Typography>
+          <Typography variant="h6" align="center" sx={{color: color}}>{character.action_values[name] - (character.impairments || 0)}</Typography>
         </Stack>
       )
     } else {
       return <></>
     }
   }
+
   return (
     <Stack direction="row" spacing={0}>
       <ActionValueDisplay label="Attack" name="Guns" character={character} />
@@ -29,7 +31,8 @@ export default function ActionValues({ character }: any) {
         <ActionValueDisplay label="Tough" name="Toughness" character={character} /> }
       { character.action_values["Type"] === "PC" &&
         <ActionValueDisplay label="Fortune" name="Fortune" character={character} /> }
-      <ActionValueDisplay label="Speed" name="Speed" character={character} sx={{...styles, borderRightColor: "#ccc", borderRight: 1}} />
+      <ActionValueDisplay label="Speed" name="Speed" character={character} />
+      <Stack sx={{borderLeft: 1, borderLeftColor: borderColor}} />
     </Stack>
   )
 }
