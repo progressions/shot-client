a45d096248f6cfbbc0401a3aadffadebb0c7d4b6
Isaac Priestley <progressions@gmail.com>
2023-01-12 11:26:39 -0500

    Include more action values

--- components/ActionValueDisplay.tsx
+++ components/ActionValueDisplay.tsx
@@ -22,7 +22,7 @@ export default function ActionValueDisplay({ name, description, label, character
     return (
       <Tooltip title={description} arrow>
         <Stack spacing={1} sx={sx || styles} border={1}>
-          <Typography variant="body2" color="white" align="center" component="div"><Box bgcolor={borderColor}>{label}</Box></Typography>
+          <Typography variant="body2" color="white" align="center" component="div"><Box bgcolor={borderColor} sx={{overflow: "hidden", whiteSpace: "nowrap"}}>{label}</Box></Typography>
           <Typography variant="h6" align="center" sx={{color: color}}>{(character.action_values[name] as number) - (character.impairments || 0)}</Typography>
         </Stack>
     </Tooltip>
