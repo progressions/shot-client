d02cafba512fe60e75e16e7e8318de61cab4dee0
Isaac Priestley <progressions@gmail.com>
2023-01-12 22:14:31 -0500

    Format values

--- components/vehicles/ActionValues.tsx
+++ components/vehicles/ActionValues.tsx
@@ -17,11 +17,11 @@ export default function ActionValues({ character }: ActionValuesParams) {
           <ActionValueDisplay name="Acceleration" description="Acceleration" label="Acceleration" character={character} />
           <ActionValueDisplay name="Handling" description="Handling" label="Handling" character={character} />
           <ActionValueDisplay name="Squeal" description="Squeal" label="Squeal" character={character} />
-          <ActionValueDisplay name="Frame" description="Frame" label="Frame" character={character} />
         </Stack>
         <Stack direction="row" spacing={1} alignItems="center">
-          <ActionValueDisplay name="Crunch" description="Crunch" label="Crunch" character={character} />
           <ActionValueDisplay name="Condition Points" description="Condition" label="Condition" character={character} />
+          <ActionValueDisplay name="Frame" description="Frame" label="Frame" character={character} />
+          <ActionValueDisplay name="Crunch" description="Crunch" label="Crunch" character={character} />
         </Stack>
       </Box>
     </>
