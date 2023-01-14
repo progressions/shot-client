fc45aa557dca97e49b8a31f4ccaa6be73a08090b
Isaac Priestley <progressions@gmail.com>
2023-01-14 14:00:03 -0500

    Don't use context yet

--- components/fights/FightName.tsx
+++ components/fights/FightName.tsx
@@ -2,17 +2,11 @@ import { Box, Typography } from "@mui/material"
 
 import type { Fight } from "../../types/types"
 
-import { useCurrentFight } from "../../contexts/CurrentFight"
-
 interface FightNameProps {
   fight: Fight
 }
 
-export default function FightName() {
-  const [fight, setFight] = useCurrentFight()
-
-  console.log(fight)
-
+export default function FightName({ fight }) {
   return (
     <>
       <Typography variant="h2" gutterBottom>
