6dabb937fa7698bb9690a010e98855cf1f5dcb03
Isaac Priestley <progressions@gmail.com>
2023-01-14 17:56:48 -0500

    Autocomplete character select

--- components/characters/SelectCharacter.tsx
+++ components/characters/SelectCharacter.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import { Box, TextField, MenuItem, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText, Button } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { IconButton, Typography, Stack, Popover, Autocomplete, Box, TextField, MenuItem, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText, Button } from '@mui/material'
 import { useSession } from 'next-auth/react'
 import { loadFight } from '../fights/FightDetail'
 import Client from "../Client"
 
 import PersonIcon from '@mui/icons-material/Person'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
 
+import { useToast } from "../../contexts/ToastContext"
 import { useFight } from "../../contexts/FightContext"
 import type { Fight, Character } from "../../types/types"
 
@@ -14,72 +16,101 @@ export default function SelectCharacter() {
   const jwt = session?.data?.authorization
   const client = new Client({ jwt })
 
+  const { setToast } = useToast()
   const [fight, setFight] = useFight()
-  const [value, setValue] = useState<string>('')
   const [open, setOpen] = useState<boolean>(false)
-  const [chars, setChars] = useState<Character[]>([])
+  const [anchorEl, setAnchorEl] = useState<any>(null)
+  const [value, setValue] = useState({label: "", id: null})
+  const [characters, setCharacters] = useState<Character[]>([])
 
-  const handleOpen = async (): Promise<void> => {
-    const response = await client.getAllCharacters()
-    const chars = await response.json()
+  useEffect(() => {
+    const getAvailableCharacters = async () => {
+      const response = await client.getAllCharacters()
+      const chars = await response.json()
+      const ids = fight.characters.map((char: Character) => char.id)
+      const availableChars = chars.filter((char: Character) => {
+        return !ids?.includes(char.id)
+      })
 
-    const ids = fight?.characters?.map((char: Character) => char.id)
-    const availableChars = chars.filter((char: Character) => {
-      return !ids?.includes(char.id)
-    })
+      const defaultOptions = [{label: "None", id: ""}]
+      const options = availableChars.map((character) => {
+        return { label: character.name, id: character.id }
+      })
 
-    setValue('')
-    setChars(availableChars)
+      const results = defaultOptions.concat(options)
+      setCharacters(results)
+    }
+
+    getAvailableCharacters()
+      .catch(console.err)
+  }, [fight])
+
+  const handleOpen = async (event) => {
     setOpen(true)
+    setAnchorEl(event.target)
   }
 
-  const handleClose = ():void => {
+  const handleChange = (event, newValue) => {
+    if (newValue) {
+      setValue(newValue)
+    } else {
+      setValue({label: "None", id: ""})
+    }
+  }
+
+  const handleClose = () => {
     setOpen(false)
-    setChars([])
-  }
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
-    setValue(event.target.value)
   }
 
   const handleSubmit = async (event: React.SyntheticEvent):Promise<void> => {
     event.preventDefault()
 
-    const response = await client.addCharacter(fight, {id: value})
-    const data = await response.json()
-    await loadFight({jwt, id: fight.id as string, setFight})
+    const { id } = value
 
-    setOpen(false)
+    if (!id) return
+
+    const response = await client.addCharacter(fight, {id: id})
+    const character = await response.json()
+    setToast({ open: true, message: `Character ${character.name} added.`, severity: "success" })
+    setValue({label: "None", id: ""})
+    await loadFight({jwt, id: fight.id as string, setFight})
   }
 
   return (
     <>
       <Button variant="outlined" startIcon={<PersonIcon />} onClick={handleOpen}>Select</Button>
-      <Dialog
+      <Popover
+        disableAutoFocus={true}
+        disableEnforceFocus={true}
         open={open}
+        anchorEl={anchorEl}
         onClose={handleClose}
-        disableRestoreFocus
-      >
-        <form onSubmit={handleSubmit}>
-        <DialogTitle>Select Character</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Select a character to add to the fight
-          </DialogContentText>
-          <Box sx={{width: 300}} py={2}>
-            <TextField label="Character" name="character" fullWidth select value={value} onChange={handleChange}>
-              {
-                chars.map((char: Character) => <MenuItem key={char.id} value={char.id}>{char.name}</MenuItem>)
-              }
-            </TextField>
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="outlined" onClick={handleClose}>Cancel</Button>
-          <Button type="submit" onClick={handleSubmit} variant="contained">Select</Button>
-        </DialogActions>
-        </form>
-      </Dialog>
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}>
+        <Box component="form" onSubmit={handleSubmit} sx={{width: 400}} p={2}>
+          <Typography variant="h4">Select Character</Typography>
+          <Stack direction="row" spacing={1}>
+            <Autocomplete
+              freeSolo
+              options={characters}
+              sx={{ width: 300 }}
+              value={value}
+              onChange={handleChange}
+              getOptionLabel={({label, id}) => label}
+              renderInput={(params) => <TextField autoFocus {...params} label="Character" />}
+            />
+            <Button type="submit" size="small" variant="contained">
+              <PersonAddIcon />
+            </Button>
+          </Stack>
+        </Box>
+      </Popover>
     </>
   )
 }
