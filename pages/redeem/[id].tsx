a790d2007d38c1b6c18cb7aacf1c958a454d9364
Isaac Priestley <progressions@gmail.com>
2023-01-16 13:47:11 -0500

    Accept invitations

--- pages/redeem/[id].tsx
+++ pages/redeem/[id].tsx
@@ -1,7 +1,9 @@
 import Layout from '../../components/Layout'
 import Head from 'next/head'
+import URL from "node:url"
 
-import { useState } from "react"
+import Router from 'next/router'
+import { useEffect, useState } from "react"
 
 import { TextField, Button, Box, Stack, TableContainer, Table, TableRow, TableHead, TableBody, TableCell, Container, Typography } from "@mui/material"
 
@@ -13,6 +15,8 @@ import CreateInvitation from "../../components/invitations/CreateInvitation"
 import Client from '../../components/Client'
 import { GetServerSideProps } from 'next'
 import Navbar from "../../components/navbar/Navbar"
+import * as cookie from 'cookie'
+import { signIn, signOut } from 'next-auth/react'
 
 import { Invitation, User, Campaign } from "../../types/types"
 
@@ -21,6 +25,7 @@ export async function getServerSideProps<GetServerSideProps>({ req, res, params 
   const jwt = session?.authorization
   const client = new Client({ jwt: jwt })
   const { id } = params
+
 
   const response = await client.getInvitation({ id })
   if (response.status === 200) {
@@ -36,7 +41,7 @@ export async function getServerSideProps<GetServerSideProps>({ req, res, params 
   return {
     redirect: {
       permanent: false,
-      destination: "/auth/signin"
+      destination: `/auth/signin`
     },
     props: {
     }
@@ -46,10 +51,19 @@ export async function getServerSideProps<GetServerSideProps>({ req, res, params 
 export default function RedeemInvitation({ invitation }: any) {
   const [user, setUser] = useState({email: invitation?.email})
   const [saving, setSaving] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const session: any = useSession({ required: false })
+
+  useEffect(() => {
+    if (invitation.pending_user.id && session.status !== "authenticated") {
+      Router.replace("/auth/signin")
+    }
+    if (session?.data?.user) {
+      setUser(session?.data?.user)
+    }
+  }, [session.status, invitation])
 
   const client = new Client()
-
-  console.log(invitation)
 
   const handleChange = (event: any) => {
     setUser((prev: any) => ({ ...prev, [event.target.name]: event.target.value }))
@@ -66,11 +80,11 @@ export default function RedeemInvitation({ invitation }: any) {
     const response = await client.redeemInvitation(invitation, user)
     if (response.status === 200) {
       const data = await response.json()
-      console.log("success", data)
+      setSuccess(true)
     } else {
-      alert("wut")
       const data = await response.json()
       console.error(data)
+      alert("error")
     }
 
     setSaving(false)
@@ -89,22 +103,40 @@ export default function RedeemInvitation({ invitation }: any) {
         <Navbar />
         <Container maxWidth="md">
           <Typography variant="h2" gutterBottom>Welcome</Typography>
-          <Typography>You've been invited to </Typography>
-          <Typography variant="h3" gutterBottom>{invitation.campaign.title}</Typography>
-          <Typography>To accept, enter your details to create an account.</Typography>
-          <Box component="form" onSubmit={handleSubmit}>
-            <Stack direction="column" spacing={2} mt={4}>
-              <TextField name="email" label="Email" value={user.email || ""} InputProps={{
-                readOnly: true,
-              }} />
-              <TextField name="first_name" label="First name" value={user.first_name || ""} onChange={handleChange} />
-              <TextField name="last_name" label="Last name" value={user.last_name || ""} onChange={handleChange} />
-              <TextField required name="password" type="password" label="Password" value={user.password || ""} onChange={handleChange} />
-              <Stack spacing={2} direction="row">
-                <Button variant="contained" type="submit" disabled={saving}>Accept</Button>
+          { success &&
+            <Box my={2}>
+              <Typography>{session?.data?.user?.email}</Typography>
+              <Typography>You have joined the campaign </Typography>
+              <Typography variant="h3" gutterBottom>{invitation.campaign.title}</Typography>
+            </Box> }
+          { !success && session.status === "authenticated" &&
+            <Box my={2}>
+              <Typography>{session?.data?.user?.email}</Typography>
+              <Typography>You've been invited to the campaign </Typography>
+              <Typography variant="h3" gutterBottom>{invitation.campaign.title}</Typography>
+              <Box component="form" onSubmit={handleSubmit}>
+                <Stack spacing={2} direction="row">
+                  <Button variant="contained" type="submit" disabled={saving}>Accept</Button>
+                </Stack>
+              </Box>
+            </Box> }
+          { !success && session.status !== "authenticated" && 
+            <Box component="form" onSubmit={handleSubmit}>
+              <Typography>You've been invited to </Typography>
+              <Typography variant="h3" gutterBottom>{invitation.campaign.title}</Typography>
+              <Typography>To accept, enter your details to create an account.</Typography>
+              <Stack direction="column" spacing={2} mt={4}>
+                <TextField name="email" label="Email" value={user?.email || ""} InputProps={{
+                  readOnly: true,
+                }} />
+                <TextField name="first_name" label="First name" value={user?.first_name || ""} onChange={handleChange} />
+                <TextField name="last_name" label="Last name" value={user?.last_name || ""} onChange={handleChange} />
+                <TextField required name="password" type="password" label="Password" value={user?.password || ""} onChange={handleChange} />
+                <Stack spacing={2} direction="row">
+                  <Button variant="contained" type="submit" disabled={saving}>Accept</Button>
+                </Stack>
               </Stack>
-            </Stack>
-          </Box>
+            </Box> }
         </Container>
       </main>
     </>
