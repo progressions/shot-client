dd50d2fbcc2238573db83b0821dedd303a69bcdf
Isaac Priestley <progressions@gmail.com>
2023-01-16 20:04:05 -0500

    Combine effects

--- components/effects/GroupedEffects.tsx
+++ components/effects/GroupedEffects.tsx
@@ -1,37 +1,35 @@
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import { Tooltip, IconButton } from "@mui/material"
+import Effects from "./Effects"
+
+const effectsForShot = (fight: Fight, shot: number) => {
+  return fight.effects.filter((effect) => {
+    return shot > 0 && (
+      (fight.sequence == effect.start_sequence && shot <= effect.start_shot) ||
+        (fight.sequence == effect.end_sequence && shot > effect.end_shot)
+    )
+  })
+}
+
+const effectsGroupedByType = (eff: any) => {
+  return eff.reduce((acc: any, effect: any) => {
+    acc[effect.severity] ||= []
+    acc[effect.severity].push(effect)
+    return acc
+  }, {})
+}
 
 export default function GroupedEffects({ fight, shot }) {
-  const effectsForShot = (fight: Fight, shot: number) => {
-    return fight.effects.filter((effect) => {
-      return shot > 0 && (
-        (fight.sequence == effect.start_sequence && shot <= effect.start_shot) ||
-          (fight.sequence == effect.end_sequence && shot > effect.end_shot)
-      )
-    })
-  }
-
-  const effectsGroupedByType = (eff: any) => {
-    return eff.reduce((acc: any, effect: any) => {
-      acc[effect.severity] ||= []
-      acc[effect.severity].push(effect)
-      return acc
-    }, {})
-  }
   const finalEffects = effectsGroupedByType(effectsForShot(fight, shot))
-
-  console.log("what")
-  console.log({ finalEffects })
-
   const severities = ["error", "warning", "info", "success"]
 
   return (
     <>
       {
         severities.map((severity) => {
-          return finalEffects[severity]?.map((effect) => {
-            return <p>{effect.title}</p>
-          })
-        }).flat()
+          if (finalEffects[severity]) {
+            return <Effects key={severity} effects={finalEffects[severity]} severity={severity} />
+          }
+       })
       }
     </>
   )
