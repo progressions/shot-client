33b8ce1f5a13e0c97fbe59fa085cc3b9f5d9ebfb
Isaac Priestley <progressions@gmail.com>
2023-01-27 09:56:11 -0500

    Move some files

--- components/characters/CharacterDetails.tsx
+++ components/characters/CharacterDetails.tsx
@@ -19,7 +19,7 @@ import GamemasterOnly from '../GamemasterOnly'
 import DeathMarks from "./DeathMarks"
 import NameDisplay from "./NameDisplay"
 import WoundsDisplay from "./WoundsDisplay"
-import GroupedEffects from "../character_effects/GroupedEffects"
+import GroupedEffects from "../state_effects/GroupedEffects"
 
 import { useFight } from "../../contexts/FightContext"
 import { useToast } from "../../contexts/ToastContext"
