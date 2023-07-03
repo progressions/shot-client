129f74c00c6e0a3b76d7d3a91dffe82cf5eeba29
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-03 09:21:19 -0400

    Remove unused references

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,32 +1,12 @@
 import Image from "next/image"
-import { Container } from "@mui/material"
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import {
-  usePopupState,
-  bindTrigger,
-  bindMenu,
-} from 'material-ui-popup-state/hooks'
 import Link from '@mui/material/Link'
-import { useEffect } from 'react'
-import Router from 'next/router'
-import { signIn, signOut } from 'next-auth/react'
-import Avatar from '@mui/material/Avatar'
-import Tooltip from '@mui/material/Tooltip'
-import Stack from '@mui/material/Stack'
 import AuthButton from "./AuthButton"
-import Client from "../../utils/Client"
 import CurrentCampaign from "../campaigns/CurrentCampaign"
-import GamemasterOnly from "../GamemasterOnly"
 import PopupMenu from "./PopupMenu"
 
-import type { Campaign, User } from "../../types/types"
 import { defaultCampaign } from "../../types/types"
 
 import { useCampaign, CampaignContextType } from "../../contexts/CampaignContext"
