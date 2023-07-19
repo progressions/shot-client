8033f718b5ba33700fff7da334187e2a1b20c38c
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-19 10:54:30 -0400

    Show category of weapon

--- components/characters/WeaponsDisplay.tsx
+++ components/characters/WeaponsDisplay.tsx
@@ -2,6 +2,7 @@ import { colors, Stack, Typography, Box, Popover, Tooltip, IconButton } from "@m
 import ArticleIcon from '@mui/icons-material/Article'
 import { useState } from "react"
 import { GiDeathSkull, GiShotgun, GiPistolGun } from "react-icons/gi"
+import WS from "../../services/WeaponService"
 
 import type { Weapon } from "../../types/types"
 
@@ -23,8 +24,6 @@ export default function WeaponsDisplay({ weapons }: WeaponsDisplayProps) {
     setOpen(true)
   }
 
-  const stats = (weapon: Weapon) => (`(${weapon.damage || "-"}/${weapon.concealment || "-"}/${weapon.reload_value || "-"})`)
-
   if (!weapons.length) return <></>
 
   return (
@@ -39,34 +38,34 @@ export default function WeaponsDisplay({ weapons }: WeaponsDisplayProps) {
           {
             weapons.map((weapon: Weapon, index: number) => (
               <Box key={weapon.name + index}>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <Typography gutterBottom sx={{color: "primary.dark"}}>
-                  <strong>{weapon.name}</strong> {stats(weapon)}
-                </Typography>
+                <Stack direction="row" spacing={1} alignItems="center">
+                  <Typography gutterBottom sx={{color: "primary.dark"}}>
+                    {WS.nameWithCategory(weapon)} {WS.stats(weapon)}
+                  </Typography>
+                    {
+                      weapon.kachunk &&
+                        <Typography sx={{color: "primary.dark"}}>
+                            <GiShotgun />
+                        </Typography>
+                    }
                   {
-                    weapon.kachunk &&
+                    weapon.mook_bonus === 1 &&
+                    <>
                       <Typography sx={{color: "primary.dark"}}>
-                          <GiShotgun />
+                        <GiDeathSkull />
                       </Typography>
+                    </>
                   }
-                {
-                  weapon.mook_bonus === 1 &&
-                  <>
-                    <Typography sx={{color: "primary.dark"}}>
-                      <GiDeathSkull />
-                    </Typography>
-                  </>
-                }
-                {
-                  weapon.mook_bonus === 2 &&
-                  <>
-                    <Typography sx={{color: "primary.dark"}}>
-                      <GiDeathSkull />
-                      <GiDeathSkull />
-                    </Typography>
-                  </>
-                }
-            </Stack>
+                  {
+                    weapon.mook_bonus === 2 &&
+                    <>
+                      <Typography sx={{color: "primary.dark"}}>
+                        <GiDeathSkull />
+                        <GiDeathSkull />
+                      </Typography>
+                    </>
+                  }
+              </Stack>
                 {
                   weapon.kachunk &&
                     <Typography gutterBottom sx={{marginTop: -1, marginLeft: 2, color: "primary.main"}} variant="subtitle2">
