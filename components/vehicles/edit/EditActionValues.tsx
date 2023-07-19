824227bf840d5cf7b5808c459b70882216959047
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-18 21:47:19 -0400

    Add descriptive helper text

--- components/vehicles/edit/EditActionValues.tsx
+++ components/vehicles/edit/EditActionValues.tsx
@@ -15,11 +15,11 @@ export default function EditActionValues({ vehicle, onChange }: EditActionValues
       <Stack spacing={2}>
         <Subhead>Action Values</Subhead>
         <Stack direction="row" spacing={2}>
-          <StyledTextField label="Acceleration" type="number" sx={{width: 100}} name="Acceleration" value={vehicle.action_values["Acceleration"] || ""} onChange={onChange} />
-          <StyledTextField label="Handling" type="number" sx={{width: 100}} name="Handling" value={vehicle.action_values?.['Handling'] || ""} onChange={onChange} />
-          <StyledTextField label="Squeal" type="number" sx={{width: 100}} name="Squeal" value={vehicle.action_values?.['Squeal'] || ""} onChange={onChange} />
-          <StyledTextField label="Frame" type="number" sx={{width: 100}} name="Frame" value={vehicle.action_values?.['Frame'] || ""} onChange={onChange} />
-          <StyledTextField label="Crunch" type="number" sx={{width: 100}} name="Crunch" value={vehicle.action_values?.['Crunch'] || ""} onChange={onChange} />
+          <StyledTextField label="Acceleration" helperText="Speed" type="number" sx={{width: 100}} name="Acceleration" value={vehicle.action_values["Acceleration"] || ""} onChange={onChange} />
+          <StyledTextField label="Handling" helperText="Chase Toughness" type="number" sx={{width: 100}} name="Handling" value={vehicle.action_values?.['Handling'] || ""} onChange={onChange} />
+          <StyledTextField label="Squeal" helperText="Chase Damage" type="number" sx={{width: 100}} name="Squeal" value={vehicle.action_values?.['Squeal'] || ""} onChange={onChange} />
+          <StyledTextField label="Frame" helperText="Condition Toughness" type="number" sx={{width: 100}} name="Frame" value={vehicle.action_values?.['Frame'] || ""} onChange={onChange} />
+          <StyledTextField label="Crunch" helperText="Condition Damage" type="number" sx={{width: 100}} name="Crunch" value={vehicle.action_values?.['Crunch'] || ""} onChange={onChange} />
         </Stack>
       </Stack>
     </>
