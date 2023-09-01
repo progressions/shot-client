bd01f1a186fd68c126eaaabc77977c0f10aedb96
Isaac Priestley <isaac.priestley@babylist.com>
2023-09-01 19:59:25 -0400

    Add image to vehicle

--- components/characters/ImageDisplay.tsx
+++ components/characters/ImageDisplay.tsx
@@ -6,7 +6,7 @@ import { StyledDialog } from "@/components/StyledFields"
 import type { Character } from "@/types/types"
 
 interface ImageDisplayProps {
-  character: Character | Vehicle
+  character: Character
 }
 
 export default function ImageDisplay({ character }: ImageDisplayProps) {
