bf54cd1dca56f651349a94a65491c7f73c769ef5
Isaac Priestley <isaac.priestley@babylist.com>
2025-06-25 16:05:57 -0400

    Use character service

--- components/characters/edit/EditActionValues.tsx
+++ components/characters/edit/EditActionValues.tsx
@@ -11,15 +11,12 @@ interface EditActionValuesProps {
 }
 
 export default function EditActionValues({ character, onChange }: EditActionValuesProps) {
-  console.log(character)
-  console.log(CS.secondaryAttack(character))
-
   return (
     <>
       <Stack spacing={2}>
         <Subhead>Action Values</Subhead>
         <Stack direction="row" spacing={2}>
-          <StyledSelect select fullWidth label="Main Attack" name="MainAttack" value={character.action_values["MainAttack"] || "Guns"} onChange={onChange}>
+          <StyledSelect select fullWidth label="Main Attack" name="MainAttack" value={CS.mainAttack(character)} onChange={onChange}>
             <MenuItem value="Guns">Guns</MenuItem>
             <MenuItem value="Martial Arts">Martial Arts</MenuItem>
             <MenuItem value="Scroungetech">Scroungetech</MenuItem>
@@ -28,7 +25,7 @@ export default function EditActionValues({ character, onChange }: EditActionValu
             <MenuItem value="Creature">Creature</MenuItem>
           </StyledSelect>
           <PlayerTypeOnly character={character} except="Mook">
-            <StyledSelect select fullWidth label="Secondary Attack" name="SecondaryAttack" value={character.action_values["SecondaryAttack"] || ""} onChange={onChange}>
+            <StyledSelect select fullWidth label="Secondary Attack" name="SecondaryAttack" value={CS.secondaryAttack(character)} onChange={onChange}>
               <MenuItem value="null">None</MenuItem>
               <MenuItem value="Guns">Guns</MenuItem>
               <MenuItem value="Martial Arts">Martial Arts</MenuItem>
@@ -40,16 +37,16 @@ export default function EditActionValues({ character, onChange }: EditActionValu
           </PlayerTypeOnly>
         </Stack>
         <Stack direction="row" spacing={2}>
-          <StyledTextField label={character.action_values["MainAttack"]} type="number" sx={{width: 100}} name={character.action_values["MainAttack"] as string} value={character.action_values[character.action_values["MainAttack"] as string] || ''} onChange={onChange} />
+          <StyledTextField label={CS.mainAttack(character)} type="number" sx={{width: 100}} name={CS.mainAttack(character)} value={CS.mainAttackValue(character)} onChange={onChange} />
           <PlayerTypeOnly character={character} except="Mook">
-            <StyledTextField label={CS.secondaryAttack(character) || ""} type="number" sx={{width: 100}} name={(character.action_values["SecondaryAttack"] || "") as string} value={CS.secondaryAttackValue(character) || ""} onChange={onChange} />
+            <StyledTextField label={CS.secondaryAttack(character) || ""} type="number" sx={{width: 100}} name={CS.secondaryAttack(character) || ""} value={CS.secondaryAttackValue(character) || ""} onChange={onChange} />
           </PlayerTypeOnly>
-          <StyledTextField label="Defense" type="number" sx={{width: 100}} name="Defense" value={character.action_values?.['Defense'] || ''} onChange={onChange} />
+          <StyledTextField label="Defense" type="number" sx={{width: 100}} name="Defense" value={CS.defense(character)} onChange={onChange} />
           <PlayerTypeOnly character={character} except="Mook">
-            <StyledTextField label="Toughness" type="number" sx={{width: 100}} name="Toughness" value={character.action_values?.['Toughness'] || ''} onChange={onChange} />
+            <StyledTextField label="Toughness" type="number" sx={{width: 100}} name="Toughness" value={CS.toughness(character)} onChange={onChange} />
           </PlayerTypeOnly>
-          <StyledTextField label="Speed" type="number" sx={{width: 100}} name="Speed" value={character.action_values?.['Speed'] || ''} onChange={onChange} />
-          <StyledTextField label="Damage" type="number" sx={{width: 100}} name="Damage" value={character.action_values?.['Damage'] || ''} onChange={onChange} />
+          <StyledTextField label="Speed" type="number" sx={{width: 100}} name="Speed" value={CS.speed(character)} onChange={onChange} />
+          <StyledTextField label="Damage" type="number" sx={{width: 100}} name="Damage" value={CS.damage(character)} onChange={onChange} />
         </Stack>
       </Stack>
     </>
