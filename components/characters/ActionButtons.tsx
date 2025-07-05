aa390308e68a2b937fa3baf96e4f49e675468485
Isaac Priestley <isaac.priestley@babylist.com>
2025-07-04 21:43:24 -0400

    Transition slide

--- components/characters/ActionButtons.tsx
+++ components/characters/ActionButtons.tsx
@@ -1,4 +1,4 @@
-import { Stack, Box, ButtonGroup, Button, Tooltip } from "@mui/material"
+import { colors, Slide, Stack, Box, ButtonGroup, Button, Tooltip } from "@mui/material"
 import BoltIcon from "@mui/icons-material/Bolt"
 import HeartBrokenIcon from "@mui/icons-material/HeartBroken"
 import EditIcon from "@mui/icons-material/Edit"
@@ -7,6 +7,9 @@ import NewReleasesIcon from "@mui/icons-material/NewReleases"
 import DirectionsRunIcon from "@mui/icons-material/DirectionsRun"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import RunCircleIcon from "@mui/icons-material/RunCircle"
+import ArrowBackIosNewSharpIcon from "@mui/icons-material/ArrowBackIosNewSharp"
+import ArrowLeftSharpIcon from '@mui/icons-material/ArrowLeftSharp';
+import ArrowRightSharpIcon from '@mui/icons-material/ArrowRightSharp';
 
 import GamemasterOnly from "@/components/GamemasterOnly"
 import PlayerTypeOnly from "@/components/PlayerTypeOnly"
@@ -30,63 +33,56 @@ interface ActionButtonsParams {
 
 export default function ActionButtons({ character, healWounds, cheeseItAction, takeWounds, takeConditionPoints, takeAction, editCharacter, deleteCharacter, takeDodgeAction }: ActionButtonsParams) {
   const { user } = useClient()
-  const [cheeseItOpen, setCheeseItOpen] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [containerRef, setContainerRef] = useState<HTMLDivElement | null>(null)
 
   const woundLabel = CS.isMook(character) ? "Kill Mooks" : "Take Smackdown"
   const woundIcon = <HeartBrokenIcon color='error' />
 
   return (
-    <Stack direction="row" spacing={1} sx={{width: 260, height: 30}} justifyContent="flex-end"  onMouseEnter={() => setCheeseItOpen(true)} onMouseLeave={() => setCheeseItOpen(false)}>
-      { cheeseItOpen && <ButtonGroup variant="contained" size="small">
-        { cheeseItAction && <Tooltip title="Cheese It" arrow>
-          <Button variant="contained" sx={{color: "black", backgroundColor: "#eb8334"}} onClick={() => cheeseItAction(character)}>
-            <RunCircleIcon sx={{width: 33, height: 33}} />
-          </Button>
-        </Tooltip> }
-      </ButtonGroup> }
-      { cheeseItOpen &&
-      <ButtonGroup variant="contained" size="small">
-        { takeWounds &&
-          <Tooltip title={woundLabel} arrow>
-            <Button onClick={() => {takeWounds(character)}}>
-              {woundIcon}
+    <>
+    <Stack direction="row" justifyContent="flex-end" sx={{height: 30, width: 350}} onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} ref={setContainerRef}>
+      <Slide direction="left" in={open} mountOnEnter unmountOnExit onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(open)} container={containerRef?.current}>
+        <Stack direction="row" spacing={1} sx={{width: 260, height: 30}} justifyContent="flex-end" ref={containerRef}>
+        <ButtonGroup variant="contained" size="small">
+          { cheeseItAction && <Tooltip title="Cheese It" arrow>
+            <Button variant="contained" sx={{color: "black", backgroundColor: "#eb8334"}} onClick={() => cheeseItAction(character)}>
+              <RunCircleIcon sx={{width: 33, height: 33}} />
             </Button>
           </Tooltip> }
-        { healWounds &&
-        <PlayerTypeOnly character={character} except="Mook">
-          <Tooltip title="Heal Wounds" arrow>
-            <Button variant="contained" onClick={() => {healWounds(character)}}>
-              <FavoriteIcon color="error" />
-            </Button>
-          </Tooltip>
-        </PlayerTypeOnly> }
-        <GamemasterOnly user={user} character={character}>
-          { editCharacter &&
-          <Tooltip title="Edit Character" arrow>
-            <Button color="secondary" onClick={() => {editCharacter(character)}}>
-              <EditIcon />
+        </ButtonGroup>
+        <ButtonGroup variant="contained" size="small">
+          { takeWounds &&
+            <Tooltip title={woundLabel} arrow>
+              <Button onClick={() => {takeWounds(character)}}>
+                {woundIcon}
+              </Button>
+            </Tooltip> }
+          { healWounds &&
+          <PlayerTypeOnly character={character} except="Mook">
+            <Tooltip title="Heal Wounds" arrow>
+              <Button variant="contained" onClick={() => {healWounds(character)}}>
+                <FavoriteIcon color="error" />
+              </Button>
+            </Tooltip>
+          </PlayerTypeOnly> }
+        </ButtonGroup>
+        <ButtonGroup variant="outlined" size="small" className="actionButtons">
+          { takeDodgeAction && <Tooltip title="Dodge" arrow>
+            <Button variant="contained" color="highlight" onClick={() => takeDodgeAction(character)}>
+              <DirectionsRunIcon />
             </Button>
           </Tooltip> }
-          { deleteCharacter &&
-          <Tooltip title="Delete Character" arrow>
-            <Button color="secondary" onClick={() => deleteCharacter(character)}>
-              <DeleteIcon />
+          { takeAction && <Tooltip title="Take Action" arrow>
+            <Button sx={{width: 60}} variant="contained" color="highlight" onClick={() => {takeAction(character)}}>
+              <BoltIcon sx={{width: 50, height: 50}} />
             </Button>
           </Tooltip> }
-        </GamemasterOnly>
-      </ButtonGroup> }
-      <ButtonGroup variant="outlined" size="small" className="actionButtons">
-        { takeDodgeAction && <Tooltip title="Dodge" arrow>
-          <Button variant="contained" color="highlight" onClick={() => takeDodgeAction(character)}>
-            <DirectionsRunIcon />
-          </Button>
-        </Tooltip> }
-        { takeAction && <Tooltip title="Take Action" arrow>
-          <Button sx={{width: 60}} variant="contained" color="highlight" onClick={() => {takeAction(character)}}>
-            <BoltIcon sx={{width: 50, height: 50}} />
-          </Button>
-        </Tooltip> }
-      </ButtonGroup>
-    </Stack>
+        </ButtonGroup>
+      </Stack>
+    </Slide>
+    { open ? <ArrowRightSharpIcon /> : <ArrowLeftSharpIcon /> }
+  </Stack>
+  </>
   )
 }
